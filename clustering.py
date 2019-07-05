24edfc3601f5551679909564899f1321841ed455
AlexMuliar <32176065+AlexMuliar@users.noreply.github.com>
2019-07-05 18:04:08 +0300

    Added Scipy optimization

--- clustering.py
+++ clustering.py
@@ -1,6 +1,8 @@
 from db_config import connection
 from PIL import Image
 from sklearn.cluster import DBSCAN
+from scipy.optimize import minimize
+
 # from cuml import DBSCAN
 # import cudf
 import pandas as pd
@@ -11,13 +13,16 @@ save_to = 'result'
 os.mkdir(save_to)
 
 size = 16, 16
-min_samples = 3
+min_samples = 40
 
+n_iter = 1
 
 def get_data():
     df = pd.read_sql("\
         SELECT id, image, width, height\
-        FROM object_image", connection)
+        FROM object_image\
+        WHERE height > 100\
+        LIMIT 10000", connection)
     for i in range(df.shape[0]):
         obj = df.iloc[i]
         df.image[i] = Image.frombytes("RGB", (obj.width, obj.height), obj.image).convert('L').resize(size)
@@ -29,9 +34,19 @@ def get_data():
 
 
 def search_distance(data):
+    def clustering(eps):
+        clf_ = DBSCAN(eps=eps, min_samples=min_samples, n_jobs=-1)
+        clf_.fit(images)
+        unique_, counts_ = np.unique(clf_.labels_, return_counts=True)
+        counts_ = [i for i in counts_]
+        if counts_[1:]:
+            return counts_[0] + counts_[counts_.index(max(counts_[1:]))]
+        else:
+            return counts_[0]
+
     distance = 1
     plot = dict()
-    step = 1
+    step = 50 ** (1 / n_iter)
     images = list()
     for i in data.image:
         image = np.array(i).reshape((size[0] * size[1]))
@@ -49,10 +64,12 @@ def search_distance(data):
             noise_number = counts[0]
         plot.update({distance: noise_number})
         distance += step
-        #print(noise_number, counts)
+        
     key_list = list(plot.keys())
     val_list = list(plot.values())
-    return key_list[val_list.index(min(val_list))]
+    minimum = key_list[val_list.index(min(val_list))]
+    res = minimize(clustering, minimum, method='nelder-mead', options={'xtol': 1e-8, 'disp': True})
+    return res.x[0]
 
 
 def marker_clusters(data, eps):
@@ -73,6 +90,8 @@ def marker_clusters(data, eps):
     return 1, data
 
 def iterator(n):
+    global n_iter
+
     data = get_data()
     max_cluster = 0
     for _ in range(n):
@@ -97,6 +116,7 @@ def iterator(n):
             item.image.save(f'{ save_to }/{ data.iloc[i].cluster + max_cluster }/{ item.id }.jpg')
         max_cluster += max(data.cluster.unique()) + 1
         data = data[data.cluster == -1]
+        n_iter += 1
     os.mkdir(f'{ save_to }/-1')
     for i in range(data.shape[0]):
         item = pd.read_sql(f"\
@@ -108,5 +128,8 @@ def iterator(n):
 
 
 if __name__ == "__main__":
+    import datetime
+    start = datetime.datetime.now()
     #os.system('rm -rf result; mkdir result;')
-    iterator(3)
+    iterator(20)
+    print(datetime.datetime.now() - start)
